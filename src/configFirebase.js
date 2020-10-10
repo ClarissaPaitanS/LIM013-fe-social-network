d233101cecad46e561405b10a20df38d53f813c4
ClarissaPaitanS <clarypaitan@gmail.com>
2020-10-10 00:18:40 -0500

    completar actualizar, creando colección post

--- src/configFirebase.js
+++ src/configFirebase.js
@@ -2,26 +2,26 @@
 // export const store = firebase.store();
 // export const auth = firebase.auth();
 // export const store = firebase.store();
-
+const auth = firebase.auth();
 // ---------------------------------------Authentication----------------------------
 // SignUp User:
 export const signupUser = (email, password) => (
-  firebase.auth().createUserWithEmailAndPassword(email, password)
+  auth.createUserWithEmailAndPassword(email, password)
 );
 
 // Login User:
 export const loginUser = (email, password) => (
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  auth.signInWithEmailAndPassword(email, password)
 );
 
 // Crea una instancia del objeto del proveedor de Google.
 export const googleLogin = () => (
-  firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+  auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
 );
 
 // Crea una instancia del objeto del proveedor de Facebook.
 export const facebookLogin = () => (
-  firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
+  auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
 );
 // Cerrar Sesion.
-export const closeSesion = () => firebase.auth().signOut();
+export const closeSesion = () => auth.signOut();
